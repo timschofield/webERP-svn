41df25e0e76fff9430509c270a8c279b9895db27
Tim Schofield <tim@weberpafrica.com>
2022-09-27 10:59:01 +0100

    Version 5.0 improvements Files changed in commit: AddCustomerTypeNotes.php On branch v5.0

--- AddCustomerTypeNotes.php
+++ AddCustomerTypeNotes.php
@@ -1,221 +1,212 @@
 <?php
-
-include('includes/session.php');
+include ('includes/session.php');
 $Title = _('Customer Type (Group) Notes');
-include('includes/header.php');
-include('includes/SQL_CommonFunctions.inc');
+include ('includes/header.php');
+include ('includes/SQL_CommonFunctions.php');
 
-if (isset($_GET['Id'])){
+if (isset($_GET['Id'])) {
 	$Id = (int)$_GET['Id'];
-} else if (isset($_POST['Id'])){
+} else if (isset($_POST['Id'])) {
 	$Id = (int)$_POST['Id'];
 }
-if (isset($_POST['DebtorType'])){
+if (isset($_POST['DebtorType'])) {
 	$DebtorType = $_POST['DebtorType'];
-} elseif (isset($_GET['DebtorType'])){
+} elseif (isset($_GET['DebtorType'])) {
 	$DebtorType = $_GET['DebtorType'];
 }
-echo '<a href="' . $RootPath . '/SelectCustomer.php?DebtorType='.$DebtorType.'">' . _('Back to Select Customer') . '</a><br />';
+echo '<div class="toplink">
+		<a href="', $RootPath, '/SelectCustomer.php?DebtorType=', urlencode($DebtorType), '">', _('Back to Select Customer'), '</a>
+	</div>';
 
-if (isset($_POST['submit']) ) {
+if (isset($_POST['submit'])) {
 
 	//initialise no input errors assumed initially before we test
 	$InputError = 0;
 	/* actions to take once the user has clicked the submit button
-	ie the page has called itself with some user input */
+	 ie the page has called itself with some user input */
 
 	//first off validate inputs sensible
 	if (!is_long((integer)$_POST['Priority'])) {
 		$InputError = 1;
-		prnMsg( _('The Contact priority must be an integer.'), 'error');
-	} elseif (mb_strlen($_POST['Note']) >200) {
+		prnMsg(_('The Contact priority must be an integer.'), 'error');
+	} elseif (mb_strlen($_POST['Note']) > 200) {
 		$InputError = 1;
-		prnMsg( _('The contacts notes must be two hundred characters or less long'), 'error');
-	} elseif( trim($_POST['Note']) == '' ) {
+		prnMsg(_('The contacts notes must be two hundred characters or less long'), 'error');
+	} elseif (trim($_POST['Note']) == '') {
 		$InputError = 1;
-		prnMsg( _('The contacts notes may not be empty'), 'error');
+		prnMsg(_('The contacts notes may not be empty'), 'error');
 	}
 
-	if ($Id and $InputError !=1) {
+	if (isset($Id) and $InputError != 1) {
 
-		$sql = "UPDATE debtortypenotes SET note='" . $_POST['Note'] . "',
+		$SQL = "UPDATE debtortypenotes SET note='" . $_POST['Note'] . "',
 											date='" . FormatDateForSQL($_POST['NoteDate']) . "',
 											href='" . $_POST['Href'] . "',
 											priority='" . $_POST['Priority'] . "'
-										WHERE typeid ='".$DebtorType."'
-										AND noteid='".$Id."'";
-		$msg = _('Customer Group Notes') . ' ' . $DebtorType  . ' ' . _('has been updated');
-	} elseif ($InputError !=1) {
+										WHERE typeid ='" . $DebtorType . "'
+										AND noteid='" . $Id . "'";
+		$Msg = _('Customer Group Notes') . ' ' . $DebtorType . ' ' . _('has been updated');
+	} elseif ($InputError != 1) {
 
-		$sql = "INSERT INTO debtortypenotes (typeid,
+		$SQL = "INSERT INTO debtortypenotes (typeid,
 											href,
 											note,
 											date,
 											priority)
-									VALUES ('" . $DebtorType. "',
+									VALUES ('" . $DebtorType . "',
 											'" . $_POST['Href'] . "',
 											'" . $_POST['Note'] . "',
 											'" . FormatDateForSQL($_POST['NoteDate']) . "',
 											'" . $_POST['Priority'] . "')";
-		$msg = _('The contact group notes record has been added');
+		$Msg = _('The contact group notes record has been added');
 	}
 
-	if ($InputError !=1) {
-		$result = DB_query($sql);
+	if ($InputError != 1) {
+		$Result = DB_query($SQL);
 
-		echo '<br />';
-		prnMsg($msg, 'success');
+		prnMsg($Msg, 'success');
 		unset($Id);
 		unset($_POST['Note']);
 		unset($_POST['NoteID']);
 	}
 } elseif (isset($_GET['delete'])) {
-//the link to delete a selected record was clicked instead of the submit button
+	//the link to delete a selected record was clicked instead of the submit button
+	// PREVENT DELETES IF DEPENDENT RECORDS IN 'SalesOrders'
+	$SQL = "DELETE FROM debtortypenotes
+			WHERE noteid='" . $Id . "'
+			AND typeid='" . $DebtorType . "'";
+	$Result = DB_query($SQL);
 
-// PREVENT DELETES IF DEPENDENT RECORDS IN 'SalesOrders'
-
-	$sql="DELETE FROM debtortypenotes
-			WHERE noteid='".$Id."'
-			AND typeid='".$DebtorType."'";
-	$result = DB_query($sql);
-
-	echo '<br />';
-	prnMsg( _('The contact group note record has been deleted'), 'success');
+	prnMsg(_('The contact group note record has been deleted'), 'success');
 	unset($Id);
 	unset($_GET['delete']);
 
 }
 
 if (!isset($Id)) {
-	$SQLname="SELECT typename from debtortype where typeid='".$DebtorType."'";
-	$result = DB_query($SQLname);
-	$myrow = DB_fetch_array($result);
-	echo '<p class="page_title_text"><img src="'.$RootPath.'/css/'.$Theme.'/images/customer.png" title="' . _('Search') . '" alt="" />'  . _('Notes for Customer Type').': <b>' .$myrow['typename'] . '</b></p>
-		<br />';
+	$SQLname = "SELECT typename from debtortype where typeid='" . $DebtorType . "'";
+	$Result = DB_query($SQLname);
+	$MyRow = DB_fetch_array($Result);
+	echo '<p class="page_title_text">
+			<img src="', $RootPath, '/css/', $_SESSION['Theme'], '/images/customer.png" title="', _('Search'), '" alt="" />', _('Notes for Customer Type'), ': <b>', $MyRow['typename'], '</b>
+		</p>';
 
-	$sql = "SELECT noteid,
+	$SQL = "SELECT noteid,
 					typeid,
 					href,
 					note,
 					date,
 					priority
 				FROM debtortypenotes
-				WHERE typeid='".$DebtorType."'
+				WHERE typeid='" . $DebtorType . "'
 				ORDER BY date DESC";
-	$result = DB_query($sql);
-			//echo '<br />' . $sql;
+	$Result = DB_query($SQL);
 
-	echo '<table class="selection">';
-	echo '<tr>
-			<th>' . _('Date') . '</th>
-			<th>' . _('Note') . '</th>
-			<th>' . _('href') . '</th>
-			<th>' . _('Priority') . '</th>
-		</tr>';
+	if (DB_num_rows($Result) > 0) {
+		echo '<table>';
+		echo '<tr>
+				<th>', _('Date'), '</th>
+				<th>', _('Note'), '</th>
+				<th>', _('href'), '</th>
+				<th>', _('Priority'), '</th>
+				<th colspan="2"></th>
+			</tr>';
 
-	while ($myrow = DB_fetch_array($result)) {
-		printf('<tr class="striped_row">
-				<td>%s</td>
-				<td>%s</td>
-				<td>%s</td>
-				<td>%s</td>
-				<td><a href="%sId=%s&amp;DebtorType=%s">' .  _('Edit') . '</a></td>
-				<td><a href="%sId=%s&amp;DebtorType=%s&amp;delete=1">' .  _('Delete') . '</a></td>
-				</tr>',
-				$myrow['date'],
-				$myrow['note'],
-				$myrow['href'],
-				$myrow['priority'],
-				htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, 'UTF-8') . '?',
-				$myrow['noteid'],
-				$myrow['typeid'],
-				htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, 'UTF-8') . '?',
-				$myrow['noteid'],
-				$myrow['typeid']);
+		while ($MyRow = DB_fetch_array($Result)) {
+			echo '<tr class="striped_row">
+				<td>', ConvertSQLDate($MyRow['date']), '</td>
+				<td>', $MyRow['note'], '</td>
+				<td>', $MyRow['href'], '</td>
+				<td>', $MyRow['priority'], '</td>
+				<td><a href="', htmlspecialchars(basename(__FILE__), ENT_QUOTES, 'UTF-8') . '?Id=', urlencode($MyRow['noteid']), '&amp;DebtorType=', urlencode($MyRow['typeid']), '">', _('Edit'), '</a></td>
+				<td><a href="', htmlspecialchars(basename(__FILE__), ENT_QUOTES, 'UTF-8') . '?Id=', urlencode($MyRow['noteid']), '&amp;DebtorType=', urlencode($MyRow['typeid']), '&amp;delete=1">', _('Delete'), '</a></td>
+			</tr>';
 
+		}
+		//END WHILE LIST LOOP
+		echo '</table>';
 	}
-	//END WHILE LIST LOOP
-	echo '</table>';
 }
+
 if (isset($Id)) {
 	echo '<div class="centre">
-			<a href="' . htmlspecialchars($_SERVER['PHP_SELF'],ENT_QUOTES,'UTF-8') . '?DebtorType=' . $DebtorType . '">' . _('Review all notes for this Customer Type')  . '</a>
+			<a href="', htmlspecialchars(basename(__FILE__), ENT_QUOTES, 'UTF-8'), '?DebtorType=', urlencode($DebtorType), '">', _('Review all notes for this Customer Type'), '</a>
 		</div>';
 }
 
 if (!isset($_GET['delete'])) {
 
-	echo '<form method="post" action="' . htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, 'UTF-8') . '?DebtorType='.$DebtorType.'">';
-    echo '<div>';
-	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
+	echo '<form method="post" action="', htmlspecialchars(basename(__FILE__), ENT_QUOTES, 'UTF-8'), '?DebtorType=', urlencode($DebtorType), '">';
+	echo '<input type="hidden" name="FormID" value="', $_SESSION['FormID'], '" />';
 
 	if (isset($Id)) {
 		//editing an existing
-
-		$sql = "SELECT noteid,
+		$SQL = "SELECT noteid,
 					typeid,
 					href,
 					note,
 					date,
 					priority
 				FROM debtortypenotes
-				WHERE noteid=".$Id."
-					AND typeid='".$DebtorType."'";
+				WHERE noteid=" . $Id . "
+					AND typeid='" . $DebtorType . "'";
 
-		$result = DB_query($sql);
-				//echo '<br />' . $sql;
+		$Result = DB_query($SQL);
 
-		$myrow = DB_fetch_array($result);
+		$MyRow = DB_fetch_array($Result);
 
-		$_POST['NoteID'] = $myrow['noteid'];
-		$_POST['Note']	= $myrow['note'];
-		$_POST['Href']  = $myrow['href'];
-		$_POST['NoteDate']  = $myrow['date'];
-		$_POST['Priority']  = $myrow['priority'];
-		$_POST['typeid']  = $myrow['typeid'];
-		echo '<input type="hidden" name="Id" value="'. $Id .'" />';
-		echo '<input type="hidden" name="Con_ID" value="' . $_POST['NoteID'] . '" />';
-		echo '<input type="hidden" name="DebtorType" value="' . $_POST['typeid'] . '" />';
-		echo '<table class="selection">
-				<tr>
-					<td>' .  _('Note ID').':</td>
-					<td>' . $_POST['NoteID'] . '</td>
-				</tr>';
+		$_POST['NoteID'] = $MyRow['noteid'];
+		$_POST['Note'] = $MyRow['note'];
+		$_POST['Href'] = $MyRow['href'];
+		$_POST['NoteDate'] = $MyRow['date'];
+		$_POST['Priority'] = $MyRow['priority'];
+		$_POST['TypeID'] = $MyRow['typeid'];
+		echo '<input type="hidden" name="Id" value="', $Id, '" />';
+		echo '<input type="hidden" name="Con_ID" value="', $_POST['NoteID'], '" />';
+		echo '<input type="hidden" name="DebtorType" value="', $_POST['TypeID'], '" />';
+		echo '<fieldset>
+				<legend>', _('Edit the customer group note'), '</legend>
+				<field>
+					<td>', _('Note ID'), ':</td>
+					<td>', $_POST['NoteID'], '</td>
+				</field>';
 	} else {
-		echo '<table class="selection">';
+		echo '<fieldset>
+				<legend>', _('New customer group note'), '</legend>';
 		$_POST['NoteID'] = '';
-		$_POST['Note']  = '';
-		$_POST['Href']  = '';
-		$_POST['NoteDate']  = Date($_SESSION['DefaultDateFormat']);
-		$_POST['Priority']  = '1';
-		$_POST['typeid']  = '';
+		$_POST['Note'] = '';
+		$_POST['Href'] = '';
+		$_POST['NoteDate'] = date('Y-m-d');
+		$_POST['Priority'] = 0;
+		$_POST['TypeID'] = '';
 	}
 
-	echo '<tr>
-			<td>' . _('Contact Group Note').':</td>
-			<td><textarea name="Note" autofocus="autofocus" required="required" rows="3" cols="32">' .  $_POST['Note'] . '</textarea></td>
-		</tr>
-		<tr>
-			<td>' .  _('Web site').':</td>
-			<td><input type="url" name="Href" value="'. $_POST['Href'].'" size="35" maxlength="100" /></td>
-		</tr>
-		<tr>
-			<td>' .  _('Date').':</td>
-			<td><input type="text" required="required" name="NoteDate" class="date" value="'. $_POST['NoteDate']. '" size="11" maxlength="10" /></td>
-		</tr>
-		<tr>
-			<td>' .  _('Priority').':</td>
-			<td><input type="text" class="number" name="Priority" value="'. $_POST['Priority'] .'" size="1" maxlength="3" /></td>
-		</tr>
-		</table>
-		<br />
+	echo '<field>
+			<label for="Note">', _('Contact Group Note'), ':</label>
+			<textarea name="Note" rows="3" cols="32" autofocus="autofocus">', $_POST['Note'], '</textarea>
+			<fieldhelp>', _('Any notes for this customer group.'), '</fieldhelp>
+		</field>
+		<field>
+			<label for="Href">', _('Web site'), ':</label>
+			<input type="text" name="Href" value="', $_POST['Href'], '" size="35" maxlength="100" />
+			<fieldhelp>', _('Any web site associated with this note.'), '</fieldhelp>
+		</field>
+		<field>
+			<label for="NoteDate">', _('Date'), ':</label>
+			<input type="text" name="NoteDate" class="date" value="', ConvertSQLDate($_POST['NoteDate']), '" size="10" maxlength="10" />
+			<fieldhelp>', _('The date for this note.'), '</fieldhelp>
+		</field>
+		<field>
+			<label for="Priority">', _('Priority'), ':</label>
+			<input type="text" name="Priority" value="', $_POST['Priority'], '" size="1" maxlength="3" />
+			<fieldhelp>', _('The priority for this note, (0-9)'), '</fieldhelp>
+		</field>
+		</fieldset>
 		<div class="centre">
-			<input type="submit" name="submit" value="'. _('Enter Information').'" />
+			<input type="submit" name="submit" value="', _('Enter Information'), '" />
 		</div>
-        </div>
-		</form>';
+	</form>';
 
 } //end if record deleted no point displaying form to add record
-
-include('includes/footer.php');
+include ('includes/footer.php');
 ?>
