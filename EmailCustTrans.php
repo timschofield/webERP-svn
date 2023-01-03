8079dbdb7bb36cb71d5c73a6576f383092be97c1
Tim Schofield <tim@weberpafrica.com>
2023-01-03 15:08:21 +0000

    Improvements for v5 interface Files changed in commit: EmailCustTrans.php On branch v5.0

--- EmailCustTrans.php
+++ EmailCustTrans.php
@@ -1,9 +1,6 @@
 <?php
 
-
 include('includes/session.php');
-include('includes/header.php');
-include('includes/SQL_CommonFunctions.inc');
 
 if ($_GET['InvOrCredit']=='Invoice'){
 	$TransactionType = _('Invoice');
@@ -12,16 +9,20 @@ if ($_GET['InvOrCredit']=='Invoice'){
 	$TransactionType = _('Credit Note');
 	$TypeCode =11;
 }
+
 $Title=_('Email') . ' ' . $TransactionType . ' ' . _('Number') . ' ' . $_GET['FromTransNo'];
+
+include('includes/header.php');
+include('includes/SQL_CommonFunctions.inc');
 
 if (isset($_POST['DoIt']) AND IsEmailAddress($_POST['EmailAddr'])){
 
 	if ($_SESSION['InvoicePortraitFormat']==0){
-		echo '<meta http-equiv="Refresh" content="0; url=' . $RootPath . '/PrintCustTrans.php?FromTransNo=' . $_POST['TransNo'] . '&PrintPDF=Yes&InvOrCredit=' . $_POST['InvOrCredit'] .'&Email=' . $_POST['EmailAddr'] . '">';
+		echo '<meta http-equiv="Refresh" content="0; url=', $RootPath, '/PrintCustTrans.php?FromTransNo=', $_POST['TransNo'], '&PrintPDF=Yes&InvOrCredit=', $_POST['InvOrCredit'] .'&Email=', $_POST['EmailAddr'], '">';
 
 		prnMsg(_('The transaction should have been emailed off') . '. ' . _('If this does not happen') . ' (' . _('if the browser does not support META Refresh') . ')' . '<a href="' . $RootPath . '/PrintCustTrans.php?FromTransNo=' . $_POST['FromTransNo'] . '&PrintPDF=Yes&InvOrCredit=' . $_POST['InvOrCredit'] .'&Email=' . $_POST['EmailAddr'] . '">' . _('click here') . '</a> ' . _('to email the customer transaction'),'success');
 	} else {
-		echo '<meta http-equiv="Refresh" content="0; url=' . $RootPath . '/PrintCustTransPortrait.php?FromTransNo=' . $_POST['TransNo'] . '&PrintPDF=Yes&InvOrCredit=' . $_POST['InvOrCredit'] .'&Email=' . $_POST['EmailAddr'] . '">';
+		echo '<meta http-equiv="Refresh" content="0; url=', $RootPath, '/PrintCustTransPortrait.php?FromTransNo=', $_POST['TransNo'], '&PrintPDF=Yes&InvOrCredit=', $_POST['InvOrCredit'] .'&Email=', $_POST['EmailAddr'], '">';
 
 		prnMsg(_('The transaction should have been emailed off. If this does not happen (perhaps the browser does not support META Refresh)') . '<a href="' . $RootPath . '/PrintCustTransPortrait.php?FromTransNo=' . $_POST['FromTransNo'] . '&PrintPDF=Yes&InvOrCredit=' . $_POST['InvOrCredit'] .'&Email=' . $_POST['EmailAddr'] . '">' . _('click here') . '</a> ' . _('to email the customer transaction'),'success');
 	}
@@ -32,17 +33,18 @@ if (isset($_POST['DoIt']) AND IsEmailAddress($_POST['EmailAddr'])){
 	prnMsg(_('The email address does not appear to be a valid email address. The transaction was not emailed'),'warn');
 }
 
-include ('includes/header.php');
+echo '<p class="page_title_text">
+		<img src="', $RootPath, '/css/', $_SESSION['Theme'], '/images/magnifier.png" title="', _('Departments'), '" alt="" />', ' ', $Title, '
+	</p>';
 
+echo '<form action="', htmlspecialchars($_SERVER['PHP_SELF'],ENT_QUOTES,'UTF-8'), '" method="post">';
+echo '<input type="hidden" name="FormID" value="', $_SESSION['FormID'], '" />';
 
-echo '<form action="' . htmlspecialchars($_SERVER['PHP_SELF'],ENT_QUOTES,'UTF-8') . '" method="post">';
-echo '<div>';
-echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
+echo '<input type="hidden" name="TransNo" value="', $_GET['FromTransNo'], '" />';
+echo '<input type="hidden" name="InvOrCredit" value="', $_GET['InvOrCredit'], '" />';
 
-echo '<input type="hidden" name="TransNo" value="' . $_GET['FromTransNo'] . '" />';
-echo '<input type="hidden" name="InvOrCredit" value="' . $_GET['InvOrCredit'] . '" />';
-
-echo '<br /><table>';
+echo '<fieldset>
+		<legend>', _('Email address to send to'), '</legend>';
 
 $SQL = "SELECT email
 		FROM custbranch INNER JOIN debtortrans
@@ -61,14 +63,13 @@ if (DB_num_rows($ContactResult)>0){
 	$EmailAddress ='';
 }
 
-echo '<tr><td>' . _('Email') . ' ' . $_GET['InvOrCredit'] . ' ' . _('number') . ' ' . $_GET['FromTransNo'] . ' ' . _('to') . ':</td>
-	<td><input type="email" name="EmailAddr" autofocus="autofocus" maxlength="60" size="60" value="' . $EmailAddress . '" /></td>
-    </tr>
-	</table>';
+echo '<field>
+		<label for="EmailAddr">', _('Email'), ' ', $_GET['InvOrCredit'], ' ', _('number'), ' ', $_GET['FromTransNo'], ' ', _('to'), ':</label>
+		<input type="email" name="EmailAddr" autofocus="autofocus" maxlength="60" size="30" value="', $EmailAddress, '" />
+    </field>
+</fieldset>';
 
-echo '<br /><div class="centre"><input type="submit" name="DoIt" value="' . _('OK') . '" />';
-echo '</div>
-      </div>
-      </form>';
+echo '<div class="centre"><input type="submit" name="DoIt" value="', _('OK'), '" /></div>';
+echo '</form>';
 include ('includes/footer.php');
 ?>
